2debf0d18b2e5e13d555b551043d7b8778e829a6
makise-homura <akemi_homura@kurisa.ch>
2024-07-04 21:33:01 +0300

    Init repo with license and empty userscript

---
+++ tabun-restore-unread.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Восстановление непрочитанных комментов на Табуне
+// @version      0.1
+// @description  Добавляет возможность "вспомнить" непрочитанные комменты после случайного обновления или закрытия страницы
+// @author       makise_homura
+// @match        https://tabun.everypony.ru/*
+// @match        https://tabun.everypony.info/*
+// @grant        none
+// ==/UserScript==
+
+(() =>
+{
+
+})();
